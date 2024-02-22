da29dc6e48c4b511a013564a92a785bf6fded705
ChocoMucho <vkclssha14@gmail.com>
2024-02-22 18:14:52 +0900

    Feat : 캐릭터 이동 기능 구현

---
+++ Assets/Scripts/Characters/Player/StateMachines/PlayerIdleState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerGroundedState
+{
+    public PlayerIdleState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        stateMachine.MovementSpeedModifier = 0f; // 기본 상태라서 속도 0으로
+        base.Enter();
+        StartAnimation(stateMachine.Player.AnimationData.IdleParameterHash);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        StopAnimation(stateMachine.Player.AnimationData.IdleParameterHash);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (stateMachine.MovementInput != Vector2.zero)
+        {
+            OnMove();
+            return;
+        }
+    }
+}
